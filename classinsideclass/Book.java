a568cf32d07fa6176a0075f0962fe452283b51e1
somya2906 <168294265+somya2906@users.noreply.github.com>
2025-01-03 18:24:45 +0530

    Create Book.java

---
+++ classinsideclass/Book.java
@@ -0,0 +1,25 @@
+package classinsideclass;
+
+public class Book {
+	int id;
+	String name;
+	String author;
+	Reviews review;  // Reviews is a separate class with some fields
+	
+	// in constructor just include the fields which are fixed and required fields to define the object state
+	public Book(int id, String name, String author) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.author = author;
+	}
+   // include getters and setters only for fields which are subjected to change
+	public Reviews getReview() {
+		return review;
+	}
+
+	public void setReview(Reviews review) {
+		this.review = review;
+	}
+
+}
